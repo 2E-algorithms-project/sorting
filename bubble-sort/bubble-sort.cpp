6df559a76eb8cedc7ff37f36e1b9d36e3d8fc16a
GabrielProstitis <84264964+GabrielProstitis@users.noreply.github.com>
2021-05-16 18:50:25 +0200

    Create bubble-sort.cpp

---
+++ bubble-sort/bubble-sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#define MAX 20
+
+using namespace std;
+
+void bubbleSort(int l[], int s)
+{
+	int temp;
+	
+	for(int i = 0; i < s; i++)
+	{
+		for(int j = 1; j < s; j++)
+		{
+			if(l[j-1] > l[j])
+			{
+				temp = l[j];
+				l[j] = l[j-1];
+				l[j-1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+   int arr[MAX];
+   int n;
+   cin >> n;
+    
+   for(int i = 0; i < n; i++)
+   {
+       cin >> arr[i];
+   }
+    
+   bubbleSort(arr, s);
+    
+   cout << "Sorted Array: " << endl;
+   for(int i = 0; i < n; i++)
+   {
+        cout << arr[i] << " ";   
+   }
+}
