8f56ad77904efb71b80123e1fbb671617a6f11cc
GabrielProstitis <84264964+GabrielProstitis@users.noreply.github.com>
2021-05-17 23:22:29 +0200

    selections sort
    Added comments

--- selection-sort/selections-sort.cpp
+++ selection-sort/selections-sort.cpp
@@ -1,42 +1,47 @@
-#include <iostream>
-#define MAX 100
+#include <iostream> // for basic Input/Output stream.
+#define MAX 100 // for max size of array (arr).
 
-using namespace std;
+using namespace std; // for not repeting std before cout, cin, endl, etc...
 
 void selectionSort(int l[], int s);
 
 int main()
 {
-  int arr[MAX];
-  int n;
-  cin >> n;
-  
-  for(int i = 0; i < n; i++)
-  {
-    cin >> arr[i]; 
-  }
-  
-  selectionSort(arr, n);
-  
-  cout << "Selection Sort: " << endl;
-  for(int i = 0; i < n; i++)
-  {
-    cout << arr[i] << " "; 
-  }
-  return 0;
+  	int arr[MAX];
+  	int n;
+  	cin >> n;
+  	
+	// Input.
+  	for(int i = 0; i < n; i++)
+  	{
+   		cin >> arr[i]; 
+  	}
+  	
+	// Sorting.
+  	selectionSort(arr, n);
+  	
+	// Output.
+  	cout << "Selection Sort: " << endl;
+  	for(int i = 0; i < n; i++)
+  	{
+  		cout << arr[i] << " "; 
+  	}
+	
+	return 0;
 }
 
 void selectionSort(int l[], int s) 
 {
-	int maxValue, maxValuePos;
-	int indexLast = s-1;
-	int temp;
+	int maxValue, maxValuePos; // for storing value and position of max (to then insert at the end of the list).
+	int indexLast = s; // (it should be -1 but we use it as boundary).
+	int temp; // for swapping values
 	
 	for(int i = 0; i < s; i++)
 	{
-		maxValue = l[0];
+		maxValue = l[0]; // giveing maxValue and maxValuePos the value 0 as default
 		maxValuePos = 0;
-		for(int j = 0; j < indexLast+1; j++)
+		
+		for(int j = 0; j < indexLast; j++)
 		{
 			if(l[j] > maxValue)
 			{
@@ -44,10 +49,12 @@ void selectionSort(int l[], int s)
 				maxValuePos = j;
 			}
 		}	
-		
-		temp = l[indexLast];
+		// Swapping.
+		temp = l[indexLast]; 
 		l[indexLast] = maxValue;
 		l[maxValuePos] = temp;
+		
+		// Moving boundary to boundary-1.
 		indexLast--;
 	}	
 }
