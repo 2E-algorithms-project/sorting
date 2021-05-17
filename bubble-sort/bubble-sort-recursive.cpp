63f61a14510e0a2f10369f4401b339adb6cd3ce7
yash312312 <yashsrivastava3@gmail.com>
2021-05-17 07:27:00 +0530

    Modified current bubblesort function

--- bubble-sort/bubble-sort-recursive.cpp
+++ bubble-sort/bubble-sort-recursive.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 // Main recursive function.
-int bubbleSortRecursive(int n, int array[]);
+void bubbleSortRecursive(int n, int array[]);
 
 // inner recursive function.
-int innerLoop(int n, int array[]);
+// int innerLoop(int n, int array[]);
 
 int main()
 {
@@ -32,35 +32,43 @@ int main()
 	return 0;
 }
 
-int bubbleSortRecursive(int n,int array[]){
-	static int i = 0;
+void bubbleSortRecursive(int n,int array[]){
 	
-	if(i<n){
-		i++;
-		return innerLoop(n,array);
+	//If the size of working array is 1 return
+	if(n == 1) 
+		return;
+
+	// In 1 pass, last element is at its sorted position
+	for(int i = 0; i < n-1; i++){ 
+		if(array[i] > array[i+1]){
+			int temp = array[i];
+			array[i] = array[i+1];
+			array[i+1] = temp;
+		}
 	}
-	
-	return 1;
+
+	// Recur for the remaining subarray
+	bubbleSortRecursive(n-1 , array); 
 }
 
-int innerLoop(int n,int array[]){
+// int innerLoop(int n,int array[]){
 	
-	static int j=0;
-	int temp;
+// 	static int j=0;
+// 	int temp;
 	
-	if(j<n-1){
-		if(array[j]>array[j+1]){
-			temp=array[j]; // Swapping Variables.
-			array[j]=array[j+1];
-			array[j+1]=temp;
-		}
+// 	if(j<n-1){
+// 		if(array[j]>array[j+1]){
+// 			temp=array[j]; // Swapping Variables.
+// 			array[j]=array[j+1];
+// 			array[j+1]=temp;
+// 		}
 			
-		j++;
+// 		j++;
 		
-		return innerLoop(n,array);
-	}
+// 		return innerLoop(n,array);
+// 	}
 	
-	j=0;
+// 	j=0;
 	
-	return bubbleSortRecursive(n,array);
-}
+// 	return bubbleSortRecursive(n,array);
+// }
